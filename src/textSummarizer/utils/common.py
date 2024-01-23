060012fc3f42c5ba204b9d2a2be4cccf974b9b8b
Rich <richardleelovestoprogram@gmail.com>
2024-01-23 13:27:49 -0500

    fixed textSummarizer/utils/common.py file

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -35,18 +35,18 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     
 
 
-    @ensure_annotations
-    def create_directories(path_to_directories: list, verbose=True):
-        """create list of directories
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
         
-        Args:
-            path_to_directories (list): list of path of directories
-            ignore_log (bool,optional): ignore if multiple dirs is to be created. Defaults to False.
-        """
-        for path in path_to_directories:
-            os.makedirs(path, exist_ok=True)
-            if verbose:
-                logger.info(f"created directory at: {path}")
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool,optional): ignore if multiple dirs is to be created. Defaults to False.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
 
 
 
