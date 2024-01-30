e24c97fb10df9fa14e2935d94f16129af698e545
Rich <richardleelovestoprogram@gmail.com>
2024-01-30 18:39:23 -0500

    fixed all errors, finally!

--- main.py
+++ main.py
@@ -6,43 +6,43 @@ project_root = os.path.dirname(os.path.abspath(__file__))
 src_path = os.path.join(project_root, 'src')
 sys.path.append(src_path)
 
+# from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+# from textSummarizer.logging import logger
+
+# STAGE_NAME = "Data Ingestion stage"
+# try:
+#     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+#     data_ingestion = DataIngestionTrainingPipeline()
+#     data_ingestion.main()
+#     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx=====x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+
+
+
+
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.logging import logger
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
-    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
-    data_ingestion = DataIngestionTrainingPipeline()
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    data_ingestion= DataIngestionTrainingPipeline()
     data_ingestion.main()
-    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx=====x")
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
 
-
-
-
-# from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-# from textSummarizer.logging import logger
-
-
-# STAGE_NAME = "Data Ingestion stage"
-# try:
-#     logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
-#     data_ingestion= DataIngestionTrainingPipeline
-#     data_ingestion.main()
-#     logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
-
-# STAGE_NAME = "Data Validation stage"
-# try:
-#     logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
-#     data_validation= DataValidationTrainingPipeline
-#     data_validation.main()
-#     logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    data_validation= DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
