ef531755cd4bb25017c0bfd93d759baa591c264e
Rich <richardleelovestoprogram@gmail.com>
2024-02-02 11:37:32 -0500

    fix stage_04 update imports

--- src/textSummarizer/pipeline/stage_04_model_trainer.py
+++ src/textSummarizer/pipeline/stage_04_model_trainer.py
@@ -1,5 +1,5 @@
 from textSummarizer.config.configuration import ConfigurationManager
-from textSummarizer.components.data_transformation import ModelTrainer
+from textSummarizer.components.model_trainer import ModelTrainer
 from textSummarizer.logging import logger
 
 
